85bd42db4d6b3e0200086d07ef9765987db2f90b
SkyG0D <silvanosilvino@hotmail.com>
2022-07-04 04:59:04 -0300

    feat: add title and favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import Head from 'next/head';
 
 import { theme } from '../styles/theme';
 
@@ -8,11 +9,18 @@ function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   const queryClient = new QueryClient();
 
   return (
-    <ChakraProvider resetCSS theme={theme}>
-      <QueryClientProvider client={queryClient}>
-        <Component {...pageProps} />
-      </QueryClientProvider>
-    </ChakraProvider>
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.png" />
+        <title>Upfi - Armazene publicamente suas imagens</title>
+      </Head>
+
+      <ChakraProvider resetCSS theme={theme}>
+        <QueryClientProvider client={queryClient}>
+          <Component {...pageProps} />
+        </QueryClientProvider>
+      </ChakraProvider>
+    </>
   );
 }
 
